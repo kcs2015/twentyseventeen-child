8d79d969fd3ec83fc554a26732ad930230544596
kcs2015 <knightclientsolutions@gmail.com>
2017-07-21 11:04:28 -0400

    Added to correct directory

---
+++ woocommerce/emails/customer-invoice.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Customer invoice email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-invoice.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     2.5.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * @hooked WC_Emails::email_header() Output the email header
+ */
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<p> Hi <?php echo($order->get_billing_first_name()); ?>, </p>
+<p> We truly appreciate you taking the time to celebrate our "BIG" day with us in beautiful Cancun Mexico on Saturday November 4th, 2017.</p>
+    <p>Attached to this email is your invoice with your current balance for your resort & travel accommodations.</p>
+<p> If you have any questions at all, please reply to this email or you can call us at 347-291-5631</p>
+
+<p>Best, <br>Richard & Jessica</p>
+<?php // if ( $order->has_status( 'pending' ) ) : ?>
+
+    <?php /*
+    <p><?php printf( __( 'An order has been created for you on %1$s. To pay for this order please use the following link: %2$s', 'woocommerce' ), get_bloginfo( 'name', 'display' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'pay', 'woocommerce' ) . '</a>' ); ?></p>
+    */
+
+    ?>
+ <?php // endif;  ?>
+
+<?php
+
+/**
+ * @hooked WC_Emails::order_details() Shows the order details table.
+ * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
+ * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
+ * @since 2.5.0
+ */
+//do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * @hooked WC_Emails::order_meta() Shows order meta data.
+ */
+//do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * @hooked WC_Emails::customer_details() Shows customer details
+ * @hooked WC_Emails::email_address() Shows email address
+ */
+//do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * @hooked WC_Emails::email_footer() Output the email footer
+ */
+//do_action( 'woocommerce_email_footer', $email );
